9ed1535a54e17d68c9ba71bca78f0c765bec2b2a
Vazhh <rg.aguirre93@gmail.com>
2023-05-21 22:32:33 -0500

    se agrega etiqueta main

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import footer1 from "./assets/vistaWeb/footer1.png";
 import footer2 from "./assets/vistaWeb/footer2.png";
 import left from "./assets/vistaWeb/left.svg";
 import right from "./assets/vistaWeb/right.svg";
-import star from "./assets/vistaWeb/star.png"
+import star from "./assets/vistaWeb/star.png";
 
 function App() {
   return (
@@ -21,69 +21,67 @@ function App() {
           <img src={menu} className="w-14" alt="" />
           <img src={logo} className="w-14  left-3 sm:w-14  2xl:w-24  " alt="" />
         </nav>
-        <div className="absolute flex flex-col ms-5 mr-5 lg:mt-[30%]  mt-[60%] items-center">
-          
-          <h1 className="text-white flex font-roboto font-bold sm:text-4xl 2xl:text-5xl text-center">
-            Your favorite comic book store <img  className="hidden lg:inline w-8 h-8" src={star} alt="" />
-          </h1>
-          
-          
-          
-          <p className="text-center ps-1 pr-1 mt-6 text-white font-roboto text-xs lg:hidden">
-            From classics to novelties, we have everything you need to immerse
-            yourself in your favorite universes. Explore our catalog and live
-            the adventure of your life.
-          </p>
-          <p className="text-center hidden lg:inline ps-1 pr-1 mt-6 text-white font-roboto text-base">
-            Explore our catalog to live the adventure of your life.
-          </p>
-          <a
-            href=""
-            className="mt-6 text-white lg:hidden bg-purple w-80 h-16 flex flex-col justify-center text-center rounded-full text-2xl font-bold"
-          >
-            Started
-          </a>
-          <a
-            href=""
-            className="mt-6 hidden lg:inline-flex text-white bg-purple w-80 h-16 flex-col justify-center text-center rounded-md text-2xl font-bold"
-          >
-            Let's go!
-          </a>
-        </div>
 
-      {/* vista web */}
-      
-        <div className="absolute hidden lg:inline-flex flex lg:mt-[60%] h-[380px] bg-white w-full items-center justify-center">
-          <div className="bg-purple flex  w-[90%] h-[265px] relative items-center rounded-md">
-            <div className="w-full flex justify-between p-3">
-              <img src={left} alt="" />
-              <img src={right} alt="" />
+        <main className="absolute w-screen">
+          <div className="flex flex-col ms-5 mr-5 lg:mt-[30%]  mt-[60%] items-center">
+            <h1 className="text-white flex font-roboto font-bold sm:text-4xl 2xl:text-5xl text-center">
+              Your favorite comic book store{" "}
+              <img className="hidden lg:inline w-8 h-8" src={star} alt="" />
+            </h1>
+            <p className="text-center ps-1 pr-1 mt-6 text-white font-roboto text-xs lg:hidden">
+              From classics to novelties, we have everything you need to immerse
+              yourself in your favorite universes. Explore our catalog and live
+              the adventure of your life.
+            </p>
+            <p className="text-center hidden lg:inline ps-1 pr-1 mt-6 text-white font-roboto text-base">
+              Explore our catalog to live the adventure of your life.
+            </p>
+            <a
+              href=""
+              className="mt-6 text-white lg:hidden bg-purple w-80 h-16 flex flex-col justify-center text-center rounded-full text-2xl font-bold"
+            >
+              Started
+            </a>
+            <a
+              href=""
+              className="mt-6 hidden lg:inline-flex text-white bg-purple w-80 h-16 flex-col justify-center text-center rounded-md text-2xl font-bold"
+            >
+              Let's go!
+            </a>
+          </div>
+
+{/* vista web */}
+          <div className="absolute hidden lg:inline-flex flex lg:mt-[19%] h-[380px] bg-white w-full items-center justify-center">
+            <div className="bg-purple flex  w-[90%] h-[265px] relative items-center rounded-md">
+              <div className="w-full flex justify-between p-3">
+                <img src={left} alt="" />
+                <img src={right} alt="" />
+              </div>
+
+              <div className="text-white w-[25%] h-[93px] ms-[65%] absolute">
+                <h1 className="font-bold text-2xl font-roboto">Shonen:</h1>
+                <p className="font-roboto text-sm">
+                  Is the manga that is aimed at adolescent boys. They are series
+                  with large amounts of action, in which humorous situations
+                  often occur. The camaraderie between members of a collective
+                  or a combat team stands out.
+                </p>
+              </div>
             </div>
-
-            <div className="text-white w-[25%] h-[93px] ms-[65%] absolute">
-              <h1 className="font-bold text-2xl font-roboto">Shonen:</h1>
-              <p className="font-roboto text-sm">
-                Is the manga that is aimed at adolescent boys. They are series
-                with large amounts of action, in which humorous situations often
-                occur. The camaraderie between members of a collective or a
-                combat team stands out.
-              </p>
+            <div className="absolute flex mb-12 mr-[41%]">
+              <img
+                src={footer1}
+                className="w-[276px] h-[306px] mr-[30%]"
+                alt=""
+              />
+              <img src={footer2} className="w-[180px] h-[270px]" alt="" />
             </div>
           </div>
-          <div className="absolute flex mb-12 mr-[41%]">
-            <img
-              src={footer1}
-              className="w-[276px] h-[306px] mr-[30%]"
-              alt=""
-            />
-            <img src={footer2} className="w-[180px] h-[270px]" alt="" />
-          </div>
-        </div>
+        </main>
+
         <footer>
           <img src="" alt="" />
         </footer>
-        
-
       </div>
     </>
   );
