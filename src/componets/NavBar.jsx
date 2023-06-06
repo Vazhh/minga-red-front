cbd2b6ada590e8802979942fc99d7d11b362fb3e
Vazhh <rg.aguirre93@gmail.com>
2023-06-06 18:22:06 -0500

    fix limites counter

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -16,7 +16,7 @@ export default function NavBar() {
     {/* {show ? <Display options={options}/> : null} */}
     {show && <Display options={options} show={show} setShow={setShow}/>}
     <nav className=" z-10 flex justify-between w-full absolute ps-3 pr-5 sm:mt-3 sm:ps-3 sm:pr-5 2xl:mt-8 2xl:ps-11 2xl:pr-11">
-          <img src={menu} onClick={()=>setShow(!show)} className="w-14" alt="" />
+          <img src={menu} onClick={()=>setShow(!show)} className="w-14 hover:cursor-pointer" alt="" />
           <img src={logo} className="w-14  left-3 sm:w-14  2xl:w-24  " alt="" />
     </nav>
     </>
