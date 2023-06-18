ca13bfa2cd316fb664fa8f89150d5ae003eb063b
natigalo <natigalo1@gmail.com>
2023-06-18 06:55:44 -0500

    emparejamiento de estilos al formulario respecto al formulario de mangas

--- src/pages/AuthorForm.jsx
+++ src/pages/AuthorForm.jsx
@@ -2,51 +2,51 @@ import profilePhoto from "/assets/vistaWeb/profilePhoto.png";
 import { Link as Anchor, useNavigate } from "react-router-dom";
 import { useRef } from "react";
 export default function AuthorForm() {
-    return (
-        <>
-          <main className="md:relative pt-[15%] pb-[20%] bg-[#EBEBEB] flex flex-col  items-center w-full h-screen">
-            
-              <div className=" bg-white flex flex-col md:w-[50vw] w-full pt-[15%] pb-[0] md:pb-[5%] items-center md:pt-[5%] lg:pt-[8%] xl:pt-[10%]">
-                <h1>New Author</h1>
-                <img src={profilePhoto} alt="" />
+  return (
+    <>
+      <main className="md:relative  pb-[20%] bg-[#EBEBEB] flex flex-col  w-full h-screen">
 
-                <form className="flex flex-col w-full items-center mt-[59px] justify-around h-full">
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    className=" border-b-2 border-gray-400 w-[50%] h-[48px]  font-roboto font-medium text-[12px] ps-[14px]"
-                  />
-                  <input
-                    type="text"
-                    placeholder="Last name"
-                    className="border-b-2 border-gray-400 w-[50%] h-[48px]  font-roboto font-medium text-[12px] ps-[14px]"
-                  />
-                  <input
-                  
-                    type="text"
-                    placeholder="City,Country"
-                    className="border-b-2 border-gray-400 w-[50%] h-[48px]  font-roboto font-medium text-[12px] ps-[14px]"
-                  />
-                  <input
-                    type="date"
-                    value=""
-                    className="border-b-2 text-gray-400 border-gray-400 w-[50%] h-[48px]  font-roboto font-medium text-[12px] ps-[14px]"
-                  />
-                  <input
-                    type="text"
-                    placeholder="url profile image"
-                    className="border-b-2 border-gray-400 w-[50%] h-[48px]  font-roboto font-medium text-[12px] ps-[14px]"
-                  />
-                  
-                  <Anchor  to={"/register"} className="text-white bg-[#4338CA] rounded-lg ">
-                    Sign up
-                  </Anchor>
-                
-                </form>
-                
-             
-            </div>
-          </main>
-        </>
-    )
+        <div className="items-center flex flex-col  mt-24 ">
+          <h1 className="font-roboto text-[36px] pb-4 ">New Author</h1>
+          <img src={profilePhoto} alt="" />
+        </div>
+        
+        <form className="flex flex-col w-full items-center mt-[28px]">
+          <input
+            type="text"
+            placeholder="Name"
+            className=" border-b-2 bg-transparent border-gray-400 w-[50%] h-[48px] font-roboto font-medium text-[19px] ps-[14px]"
+          />
+          <input
+            type="text"
+            placeholder="Last name"
+            className="border-b-2 bg-transparent border-gray-400 w-[50%] h-[48px] font-roboto font-medium text-[19px] ps-[14px]"
+          />
+          <input
+            type="text"
+            placeholder="City,Country"
+            className="border-b-2 bg-transparent border-gray-400 w-[50%] h-[48px] font-roboto font-medium text-[19px] ps-[14px]"
+          />
+          <input
+            type="date"
+            value=""
+            className="border-b-2 bg-transparent border-gray-400 w-[50%] h-[48px] font-roboto font-medium text-[19px] ps-[14px]"
+          />
+          <input
+            type="text"
+            placeholder="url profile image"
+            className="border-b-2 bg-transparent border-gray-400 w-[50%] h-[48px] font-roboto font-medium text-[19px] ps-[14px]"
+          />
+
+          <Anchor to={"/register"} className="mt-[58px] pt-4 w-[50%] h-[68px] bg-[#4338CA] rounded-full font-roboto font-bold text-[24px] text-white text-center ">
+            Sign up
+          </Anchor>
+
+        </form>
+
+
+
+      </main>
+    </>
+  )
 } 
