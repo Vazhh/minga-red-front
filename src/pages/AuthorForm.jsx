524d9d2a197a77fbc027e21df01fe544179c24a0
natigalo <natigalo1@gmail.com>
2023-06-18 08:06:03 -0500

    se define data para authorCreate y se enlaza con las ref

--- src/pages/AuthorForm.jsx
+++ src/pages/AuthorForm.jsx
@@ -1,7 +1,35 @@
 import profilePhoto from "/assets/vistaWeb/profilePhoto.png";
 import { Link as Anchor, useNavigate } from "react-router-dom";
 import { useRef } from "react";
+import axios from "axios";
+
 export default function AuthorForm() {
+
+  const authorCreate = ()=>{
+    console.log(name)
+    console.log(last_name)
+    console.log(city)
+    console.log(country)
+    console.log(date)
+    console.log(photo)
+
+    let data = {
+      name: name.current.value,
+      last_name: last_name.current.value,
+      city: city.current.value,
+      country: country.current.value,
+      date: date.current.value,
+      photo: photo.current.value,
+    };
+  };
+  
+  const name = useRef();
+  const last_name = useRef();
+  const city = useRef();
+  const country = useRef();
+ /*  const arrayCityPlace =place,split('city,country'); console.log(place) */
+  const date = useRef();
+  const photo = useRef();
   return (
     <>
       <main className="md:relative  pb-[20%] bg-[#EBEBEB] flex flex-col  w-full h-screen">
@@ -14,33 +42,36 @@ export default function AuthorForm() {
         <form className="flex flex-col w-full items-center mt-[28px]">
           <input
             type="text"
+            ref={name}
             placeholder="Name"
             className=" border-b-2 bg-transparent border-gray-400 w-[50%] h-[48px] font-roboto font-medium text-[19px] ps-[14px]"
           />
           <input
             type="text"
+            ref={last_name}
             placeholder="Last name"
             className="border-b-2 bg-transparent border-gray-400 w-[50%] h-[48px] font-roboto font-medium text-[19px] ps-[14px]"
           />
           <input
             type="text"
+            ref={city,country}
             placeholder="City,Country"
             className="border-b-2 bg-transparent border-gray-400 w-[50%] h-[48px] font-roboto font-medium text-[19px] ps-[14px]"
           />
           <input
             type="date"
+            ref={Date}
             value=""
             className="border-b-2 bg-transparent border-gray-400 w-[50%] h-[48px] font-roboto font-medium text-[19px] ps-[14px]"
           />
           <input
             type="text"
+            ref={photo}
             placeholder="url profile image"
             className="border-b-2 bg-transparent border-gray-400 w-[50%] h-[48px] font-roboto font-medium text-[19px] ps-[14px]"
           />
-
-          <Anchor to={"/register"} className="mt-[58px] pt-4 w-[50%] h-[68px] bg-[#4338CA] rounded-full font-roboto font-bold text-[24px] text-white text-center ">
-            Sign up
-          </Anchor>
+{/* faltaria el active:true segun el schema del back */}
+          <input type="button" value="send" onClick={authorCreate} className="mt-[58px] w-[50%] h-[68px] bg-[#4338CA] rounded-full font-roboto font-bold text-[24px] text-white"/>
 
         </form>
 
