f87d731dea7226794a175b48b2620fd3bd659964
Vazhh <rg.aguirre93@gmail.com>
2023-07-03 19:45:46 -0500

    fix goggle icon

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,37 +1,44 @@
 import logo from "/assets/vistaWeb/logofooter.svg";
+import google from "/assets/vistaWeb/Google.svg";
 import imgsignin from "/assets/vistaWeb/singin.png";
-import { Link as Anchor } from "react-router-dom"
-import { useRef } from "react"
-import axios from "axios"
-import apiUrl from "../apiUrl"
-import Swal from "sweetalert2"
+import { Link as Anchor } from "react-router-dom";
+import { useRef } from "react";
+import axios from "axios";
+import apiUrl from "../apiUrl";
+import Swal from "sweetalert2";
 
 export default function SignIn() {
-
-  const email = useRef()                //defino una referencia
-  const password = useRef()
+  const email = useRef(); //defino una referencia
+  const password = useRef();
 
   const signin_data = () => {
     let data = {
-      email: email.current.value,       //accedo al valor de una referencia
-      password: password.current.value
-    }
+      email: email.current.value, //accedo al valor de una referencia
+      password: password.current.value,
+    };
     //console.log(data)
-    axios.post(apiUrl+'/auth/signin',data)
-      .then(res=> {
-        localStorage.setItem('token',res.data.response.token)
-        localStorage.setItem('user',JSON.stringify(res.data.response.user))
+    axios
+      .post(apiUrl + "/auth/signin", data)
+      .then((res) => {
+        localStorage.setItem("token", res.data.response.token);
+        localStorage.setItem("user", JSON.stringify(res.data.response.user));
       })
-      .then(()=>Swal.fire({
-        icon: 'success'
-        }))
-      .then(()=>window.location.replace('/'))
-      .catch(err=>Swal.fire({
-        icon: 'error',
-        text: 'sign in please!',
-        html: err.response.data.messages.map(each=>`<p>${each}</p>`).join('')
-      }))
-  }
+      .then(() =>
+        Swal.fire({
+          icon: "success",
+        })
+      )
+      .then(() => window.location.replace("/"))
+      .catch((err) =>
+        Swal.fire({
+          icon: "error",
+          text: "sign in please!",
+          html: err.response.data.messages
+            .map((each) => `<p>${each}</p>`)
+            .join(""),
+        })
+      );
+  };
 
   return (
     <>
@@ -53,7 +60,7 @@ export default function SignIn() {
             </p>
             <form className="flex flex-col w-full items-center mt-[59px]">
               <input
-                ref={email}             //asigno una referencia
+                ref={email} //asigno una referencia
                 type="email"
                 placeholder="Email"
                 className="border-2 border-gray-400 w-[50%] h-[48px] rounded-[10px] font-roboto font-medium text-[12px] ps-[14px]"
@@ -68,13 +75,17 @@ export default function SignIn() {
                 onClick={signin_data}
                 type="button"
                 value="Sign In"
-                className="mt-[13px] w-[50%] h-[48px] bg-[#4338CA] rounded-[10px] font-roboto font-bold text-[14px] text-white"
+                className="hover:cursor-pointer mt-[13px] w-[50%] h-[48px] bg-[#4338CA] rounded-[10px] font-roboto font-bold text-[14px] text-white"
               />
-              <input
+
+              <a
+                href="https://accounts.google.com/v3/signin/identifier?dsh=S-1945593188%3A1688431387052498&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&ifkv=AeDOFXh333ESPpierC-U9AFHbwabu7NXKW3GDPpiyQmMsS8m6FCT5GG60vBW8FSn5d5WsW6re4P4&osid=1&passive=1209600&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
                 type="button"
-                value="Sign in with Google"
-                className="mt-[16px] font-roboto font-medium text-[14px] w-[50%] h-[48px] text-gray-400 border-2 border-gray-400 rounded-[10px]"
-              />
+                className="hover:cursor-pointer flex items-center justify-center mt-[16px] font-roboto font-medium text-[14px] w-[50%] h-[48px] text-gray-400 border-2 border-gray-400 rounded-[10px]"
+              >
+                <img src={google} alt=""  className="mr-2"/>
+                <p>Sign in with Google</p> 
+              </a>
             </form>
             <p className="mt-[16px] font-roboto font-medium text-[14px]">
               you dont have an account yet?{" "}
