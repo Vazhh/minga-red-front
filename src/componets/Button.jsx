be32006dcf5a97d88259ec5e38f0824931e587f4
Guillermo Duque <guille980627@gmail.com>
2023-07-18 22:14:48 -0500

    props assigned

--- src/componets/Button.jsx
+++ src/componets/Button.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
-export default function Button() {
+export default function Button({ onClick ,value }) {
   return (
-    <input type="button" value='send' className="mt-[58px] w-[50%] h-[68px] bg-[#4338CA] rounded-full font-roboto font-bold text-[24px] text-white"/>
+    <input type="button" value={value} onClick={onClick } className="mt-[58px] w-[50%] h-[68px] bg-[#4338CA] rounded-full font-roboto font-bold text-[24px] text-white"/>
   )
 }
