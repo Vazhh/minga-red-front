f60d406b22164aacc7c19cf11408134631a4eae5
Guillermo Duque <guille980627@gmail.com>
2023-07-18 22:22:26 -0500

    Button component added

--- src/componets/Button.jsx
+++ src/componets/Button.jsx
@@ -1,5 +1,5 @@
 export default function Button({ onClick ,value }) {
   return (
-    <input type="button" value={value} onClick={onClick } className="mt-[58px] w-[50%] h-[68px] bg-[#4338CA] rounded-full font-roboto font-bold text-[24px] text-white"/>
+    <input type="button" value={value} onClick={onClick } className="mt-[58px] w-[50%] h-[68px] bg-[#4338CA] rounded-full font-roboto font-bold text-[24px] text-white cursor-pointer"/>
   )
 }
