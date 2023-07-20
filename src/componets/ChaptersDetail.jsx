d01a98e6bbee7cd3c722469a451aa915ae049674
natigalo <natigalo1@gmail.com>
2023-07-20 18:13:06 -0500

    componentes defined

--- src/componets/ChaptersDetail.jsx
+++ src/componets/ChaptersDetail.jsx
@@ -1,5 +1,18 @@
+import naruto from '/assets/vistaMobile/naruto.png'
 export default function ChaptersDetails() {
 return (
-    <p></p>
+    <div className='flex justify-center items-center mx-auto mt-8 w-[300px] bg-white justify-around '>
+                <img src={naruto} alt="" className='w-14'/>
+                <div className=' flex  flex-col items-center'>
+                    <h1>Chapter #1</h1>
+                    <div>
+                        <img src="" alt="" />
+                        <h2>169</h2>
+                    </div>
+                       
+                </div>
+                <input type="button" value="read" className="flex   h-[68px] bg-[#4338CA] rounded-2xl font-roboto font-bold text-[24px] text-white cursor-pointer"/>
+     
+            </div>
 )
 }
--- src/pages/MangaDetail.jsx
+++ src/pages/MangaDetail.jsx
@@ -1,11 +1,14 @@
 import TextBodyMangaD from '../componets/TextBodyMangaD.jsx'
-import MangaDetail from '../componets/TextBodyMangaD.jsx';
+import ChaptersDetails from '../componets/ChaptersDetail';
+import MangaDetails from '../componets/MangasDetail.jsx';
+
 
 export default function MangaDetail() {
     return (
         <main className="  pb-[20%] bg-[#EBEBEB] p-4 flex flex-col  w-screen h-full" >
-            <TextBodyMangaD />
-            <MangaDetail />
+         <TextBodyMangaD/>
+         <MangaDetails/>
+         <ChaptersDetails/>
         </main>
 
     );
