3e82f1490950eaf35ad4f6496d62deeae2a0e836
Guillermo Duque <guille980627@gmail.com>
2023-07-31 15:18:22 -0500

    Se conecta reductor de mangas_news

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mangaReducer from "./reducers/manga";
 import authorsReducer from './reducers/me_authors'
+import mangasnewsReducer from './reducers/mangas_news'
 
 const store = configureStore({
     reducer:{
         //acá vamos a traernos los reductores de los diferentes recursos (usuarios, mangas capitulos...)
         manga: mangaReducer,
-        meauthors: authorsReducer
+        meauthors: authorsReducer,
+        mangasnews: mangasnewsReducer
     }
 })
 
