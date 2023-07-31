7e68d339b14901e3b37cde7913aabe4b2220ead6
Guillermo Duque <guille980627@gmail.com>
2023-07-31 14:48:56 -0500

    Se conecta reducer de authors

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mangaReducer from "./reducers/manga";
+import authorsReducer from './reducers/me_authors'
 
 const store = configureStore({
     reducer:{
         //acá vamos a traernos los reductores de los diferentes recursos (usuarios, mangas capitulos...)
-        manga: mangaReducer
+        manga: mangaReducer,
+        meauthors: authorsReducer
     }
 })
 
